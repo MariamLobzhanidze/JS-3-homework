2c0cf3fea09cfeae0137a3a8575d010a360ade6a
Mariam Lobzhanidze <163752402+MariamLobzhanidze@users.noreply.github.com>
2024-05-02 09:49:57 +0400

    homework 3-4

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ for(let i = 1; i < 100; i++){
     console.log(i);
 };
 
+
 // homework 2
 
 let n = 1;
@@ -11,3 +12,44 @@ while (n < 50) {
     console.log(n);
     n +=1;
 };
+
+
+// homework 3
+
+const namesArray = ['mariam', 'davit', 'anna', 'leri', 'dachi'];
+
+namesArray.shift();
+console.log(namesArray);
+
+namesArray.unshift('elene');
+console.log(namesArray);
+
+namesArray.pop();
+console.log(namesArray);
+
+namesArray.push('tamo', 'liliana', 'mariami');
+console.log(namesArray);
+
+// homework 4
+
+const emptyArray =  [];
+
+ for (let x = 0; x < 10000; x++) {
+       emptyArray.push(x*x);
+ };
+ 
+ console.log(emptyArray);
+
+
+const emptyArray2 = []; 
+let k=0;
+ 
+ while (k<10000) {
+  let arrayNumber=k*k;
+  emptyArray2.push(arrayNumber)
+  k +=1;
+ };
+ 
+ console.log(emptyArray2);
+
+ 
